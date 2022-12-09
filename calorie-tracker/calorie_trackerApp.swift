f008d2b4954ba87c10ca87c998f2a9aaed275ff6
David Patterson-Cole <dpattersoncole@gmail.com>
2022-12-09 13:58:55 -0500

    Initial Commit

---
+++ calorie-tracker/calorie_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  calorie_trackerApp.swift
+//  calorie-tracker
+//
+//  Created by David Patterson-Cole on 2022-12-09.
+//
+
+import SwiftUI
+
+@main
+struct calorie_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
